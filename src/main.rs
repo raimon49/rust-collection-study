397ac1395fc508e68cb8b97af53e85bcbcd62107
raimon <raimon49@hotmail.com>
2021-06-15 20:16:27 +0900

    Add a code about create Vec<T>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    {
+        let mut _numbers: Vec<i32> = vec![];
+        let _words = vec!["step", "on", "no", "pets"];
+        let mut _buffer = vec![0u8; 1024];
+    }
 }
