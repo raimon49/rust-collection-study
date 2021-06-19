4f387b3486e5fbdc996c980de015fadcd9a45b7f
raimon <raimon49@hotmail.com>
2021-06-19 17:37:43 +0900

    Add a code about convert from HashSet to Vector

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,12 @@ fn main() {
         let mut _numbers: Vec<i32> = vec![];
         let _words = vec!["step", "on", "no", "pets"];
         let mut _buffer = vec![0u8; 1024];
+
+        // 他のコレクションからベクタに変換
+        use std::collections::HashSet;
+        let mut my_set = HashSet::new();
+        my_set.insert("A Dance With Dragons".to_string());
+        my_set.insert("To Kill a Mockingbird".to_string());
+        let _converted_my_vec = my_set.into_iter().collect::<Vec<String>>();
     }
 }
