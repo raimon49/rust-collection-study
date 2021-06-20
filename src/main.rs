bb93d5b307b32d6a69c6d704fc55b37a9cefaf5c
raimon <raimon49@hotmail.com>
2021-06-20 13:02:16 +0900

    Renaming for use variable

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     {
         let mut _numbers: Vec<i32> = vec![];
-        let _words = vec!["step", "on", "no", "pets"];
+        let words = vec!["step", "on", "no", "pets"];
         let mut _buffer = vec![0u8; 1024];
 
         // 他のコレクションからベクタに変換
